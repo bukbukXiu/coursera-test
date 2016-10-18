b8ff6d47fe642a899f3ca5c16dd4c771670c71ba
Andrew Nadezhdin <Andrew Nadezhdin>
2016-10-18 03:13:14 +0300

    caught a flu

---
+++ site/script/script.js
@@ -0,0 +1,7 @@
+var x="Hello World";
+
+function sayHello () {
+	var name = document.getElementById("name").value;
+	var message = "Hello, " + name + "!";
+	document.getElementById("content").textContent = message;
+}
